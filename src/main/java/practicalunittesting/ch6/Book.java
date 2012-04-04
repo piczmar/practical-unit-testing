2a8972b1f340b5c933d727113dda1d5c750cc546
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-04-05 01:22:48 +0200

    Ch. 6 - Custom FEST matchers

---
+++ src/main/java/practicalunittesting/ch6/Book.java
@@ -0,0 +1,17 @@
+package practicalunittesting.ch6;
+
+public class Book {
+	private String title;
+
+	public Book(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
