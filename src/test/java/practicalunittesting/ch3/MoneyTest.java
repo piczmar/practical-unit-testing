69f3e1192f540fafd0465289a01c7d419b6af51b
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-04-04 15:20:49 +0200

    Ch. 3.6.3 data providers class

--- src/test/java/practicalunittesting/ch3/MoneyTest.java
+++ src/test/java/practicalunittesting/ch3/MoneyTest.java
@@ -1,17 +1,12 @@
 package practicalunittesting.ch3;
 
-import org.testng.annotations.DataProvider;
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 public class MoneyTest {
 
-	@DataProvider
-	private static final Object[][] getMoney() {
-		return new Object[][] { { 10, "USD" }, { 20, "USD" } };
-	}
-
-	@Test(dataProvider = "getMoney")
+	@Test(dataProviderClass = DataProviders.class, dataProvider = "getMoney")
 	public void constructorShouldSetAmountAndCurrency(int amount, String currency) {
 		Money money = new Money(amount, currency);
 
