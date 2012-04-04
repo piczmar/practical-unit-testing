eb851bde0fa739e6f136ec2604dc4872cdc281ab
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-04-05 01:22:48 +0200

    Ch. 6 - Custom FEST matchers

---
+++ src/test/java/practicalunittesting/ch6/BookFestTest.java
@@ -0,0 +1,18 @@
+package practicalunittesting.ch6;
+
+import static org.fest.assertions.Assertions.*;
+import static practicalunittesting.ch6.BookAssert.*;
+import org.testng.annotations.Test;
+
+public class BookFestTest {
+
+	@Test
+	public void shouldHaveTitle() {
+		final String ANY_BOOK_TITLE = "Any tite";
+		Book book = new Book(ANY_BOOK_TITLE);
+
+		assertThat(book).hasTitle(ANY_BOOK_TITLE);
+
+	}
+
+}
