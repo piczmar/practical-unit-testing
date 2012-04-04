d7c6d05ac180716b097c67377ee08a2311c38f2a
Marcin Piczkowski <marcin.piczkowski@gmail.com>
2012-04-04 15:16:32 +0200

    TestNG parametrized test

--- src/test/java/practicalunittesting/ch3/MoneyTest.java
+++ src/test/java/practicalunittesting/ch3/MoneyTest.java
@@ -1,16 +1,22 @@
 package practicalunittesting.ch3;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class MoneyTest {
 
-	@Test
-	public void constructorShouldSetAmountAndCurrency() {
-		Money money = new Money(10, "USD");
+	@DataProvider
+	private static final Object[][] getMoney() {
+		return new Object[][] { { 10, "USD" }, { 20, "USD" } };
+	}
 
-		assertEquals(money.getAmount(), 10);
-		assertEquals(money.getCurrency(), "USD");
+	@Test(dataProvider = "getMoney")
+	public void constructorShouldSetAmountAndCurrency(int amount, String currency) {
+		Money money = new Money(amount, currency);
+
+		assertEquals(money.getAmount(), amount);
+		assertEquals(money.getCurrency(), currency);
 	}
 
 }
